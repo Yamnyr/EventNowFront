fe66b83bfb8c20d197c82cba5ee1e6d566a4d592
PrsJulien <83194514+PrsJulien@users.noreply.github.com>
2024-05-15 11:43:52 +0200

    Ajout du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import '../index.css';
+import { FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col text-start">
+                        <div className="container-gauche">Event Now</div>
+                    </div>
+                    <div className="col text-center">
+                        <div className="container-milieu">
+                            <FaInstagram size={30} />
+                            <FaLinkedin size={30} />
+                            <FaYoutube size={30} />
+                        </div>
+                    </div>
+                    <div className="col text-end ml-auto">
+                        <div className="container-droit">Copyright © 2024 Event Now. Tous droits réservés</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
